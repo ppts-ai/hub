38c6d8d1e076f10e31f2444da7c39e5fbc1b1b51
p2pmax <jasonhao518@gmail.com>
2024-09-24 13:35:12 +0200

    Update Modal.tsx

--- web/src/layout/package/installation/Modal.tsx
+++ web/src/layout/package/installation/Modal.tsx
@@ -64,7 +64,7 @@ const InstallationModal = (props: Props) => {
   }
   const onOpenModal = () => {
     if (!isNull(installMethods) && installMethods.methods.length > 0) {
-      if(window.__TAURI__) {
+      if(window.parent && window.parent.__TAURI__) {
         console.log("do installation");
         if(window.parent) {
           window.parent.postMessage("installation", "*");
